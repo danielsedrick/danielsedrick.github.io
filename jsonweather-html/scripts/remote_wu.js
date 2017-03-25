5a89b96da8d3ca4743867d1c6f004c37fd121128
danielsedrick <daniel.n.sedrick@gmail.com>
2017-03-25 15:27:45 -0600

    Week 12

--- jsonweather-html/scripts/remote_wu.js
+++ jsonweather-html/scripts/remote_wu.js
@@ -1,55 +1,55 @@
 // Current Location Scripts
 $(function () {
 
-  var status = $('#status');
+    var status = $('#status');
 
-  (function getGeoLocation() {
-    status.text('Getting Location...');
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(function (position) {
-        var lat = position.coords.latitude;
-        var long = position.coords.longitude;
+    (function getGeoLocation() {
+        status.text('Getting Location...');
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function (position) {
+                var lat = position.coords.latitude;
+                var long = position.coords.longitude;
 
-        // Call the getData function, send the lat and long
-        getData(lat, long);
+                // Call the getData function, send the lat and long
+                getData(lat, long);
 
-      });
-    } else {
-      status.text("Your browser doesn't support Geolocation or it is not enabled!");
-    }
-
-  })();
-
-  // Get the data from the wunderground API
-  function getData(lat, long){
-    $.ajax({
-        url: "http://api.wunderground.com/api/73c1c2fff6f518d1/geolookup/q/CA/San_Francisco.json",
-        dataType: "jsonp",
-        success: function (data) {
-            console.log(data);
-            var location = data.current_observation.location;
-            var temp_f = data.current_observation.temp_f;
-            console.log("Current temperature in " + location + " is: " + temp_f);
-
-            var heading = $('#heading');
-            var h2 = $('#h2');
-            var para = $('#para');
-
-            heading.html(location);
-            h2.html(temp_f);
-            para.html("Current temperature in " + location + " is: " + temp_f);
+            });
+        } else {
+            status.text("Your browser doesn't support Geolocation or it is not enabled!");
         }
-    })
+
+    })();
+
+    // Get the data from the wunderground API
+    function getData(lat, long){
+        $.ajax({
+            url:"https://api.wunderground.com/api/66f2cca3c6759655/geolookup/conditions/q/" + lat + "," +long + ".json",
+            dataType:"jsonp",
+            success: function(data){
+                console.log(data);
+                var location = data.location.city + ", " + data.location.state;
+                var temp =Math.round(data.current_observation.temp_f);
+                var summary = data.current_observation.weather;
+                var humidity = data.current_observation.relative_humidity;
+                var feels_like = data.current_observation.feelslike_f;
+                var uv = data.current_observation.UV;
+                $("#cityDisplay").text(location);
+                $("#currentTemp").text(temp + "°f");
+                $("#summary").text(summary);
+                $("#add1").text("Humidity: " + humidity);
+                $("#add2").text("Feels like " + feels_like);
+                $("#add3").text("UV rating: " + uv);
 
 
-      $("#cover").fadeOut(250);
+
+                $("#cover").fadeOut(250);
+            }
+        });
+
     }
-           });
 
-  }
-
-  // A function for changing a string to TitleCase
-  function toTitleCase(str){
-    return str.replace(/\w+/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
-  }
+    // A function for changing a string to TitleCase
+    function toTitleCase(str){
+        return str.replace(/\w+/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+    }
 });
---
+++ search/ourjava.js
@@ -0,0 +1,71 @@
+$('#query').keyup(function() {
+    var value = $('#query').val();
+    var rExp = new RegExp(value, "i");
+    $.getJSON("//autocomplete.wunderground.com/aq?query=" + value + "&cb=?", function(data) {
+        console.log(data); // test for JSON received
+        // Begin building output
+        var output = '<ol>';
+        $.each(data.RESULTS, function(key, val) {
+            if (val.name.search(rExp) != -1) {
+                output += '<li>';
+                output += '<a href="#cityDisplay" title="See results for ' + val.name + '">' + val.name + '</a>';
+                output += '</li>';
+            }
+        }); // end each
+        output += '</ol>';
+        $("#searchResults").html(output); // send results to the page
+    }); // end getJSON
+}); // end onkeyup
+
+$("#searchResults").on("click", "a", function (evt) {
+    evt.preventDefault();
+    // With the text value get the needed value from the weather.json file
+    var jsonCity = $(this).text(); // Franklin, etc...
+    console.log(jsonCity);
+    $.ajax({
+        url: "//api.wunderground.com/api/73c1c2fff6f518d1/geolookup/conditions/q/"
+        + jsonCity + ".json"
+        , dataType: "jsonp"
+        , success: function (data) {
+            console.log(data);
+            var location = data.location.city + ', ' + data.location.state;
+            var temp_f = data.current_observation.temp_f;
+            var feelslike_f = data.current_observation.feelslike_f;
+            console.log('Location is: ' + location);
+            console.log('Temp is: ' + temp_f);
+            $("#feelslike").html("Feels Like: " + feelslike_f);
+            $("#cityDisplay").text(location);
+            $("title").html(location + " | Weather Center");
+            $("#currentTemp").html(Math.round(temp_f) + '°F');
+            $("#summary").text(toTitleCase(data.current_observation.icon));
+            $("#cover").fadeOut(250);
+            console.log(data);
+
+
+        }
+
+    });
+    $.ajax({
+        url: "//api.wunderground.com/api/73c1c2fff6f518d1/forecast/q/"
+        + jsonCity + ".json"
+        , dataType: "jsonp"
+        , success: function (data) {
+            console.log(data);
+            var high = data.forecast.simpleforecast.forecastday[0].high.fahrenheit;
+            var low = data.forecast.simpleforecast.forecastday[0].low.fahrenheit;
+
+            console.log(high);
+            $("#high").html("High Temperature: " + high);
+            $("#low").html("Low Temperature: " + low);
+        }
+
+    })
+    $("#searchResults").html("");
+});
+
+
+
+function toTitleCase(str){
+    return str.replace(/\w+/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+}
+
